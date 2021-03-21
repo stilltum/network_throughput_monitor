a86bc8ee0e9e39bf1350f442033dcc16d3a77aa8
stilltum <35137975+stilltum@users.noreply.github.com>
2021-03-21 18:03:33 +0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -10,57 +10,45 @@ import numpy as np
 #
 #    {...}]
 # so this array gets updated by chopping the end and prepending the new minute
-def get_graph_height(outgoing_packets):
-    graph_height = 0.0
-    for packet in outgoing_packets:
-        hits = 0.0
-        for hit in packet.values():
-            hits += hit
-        if hits > graph_height:
-            graph_height = hits
+def get_packet_data(timeslot_packets):
+    # Get all addresses across the time window
+    all_addresses = set()
+    timeslot_addresses = list(map(lambda packet: [*packet], timeslot_packets))
+    for timeslot in timeslot_addresses:
+        for address in timeslot:
+            all_addresses.add(address)
+    all_addresses = [*all_addresses]
 
-    return graph_height
-
-def get_percentages(all_addresses, timeslot_packets, height):
-    packet_percentages = {}
+    # Zero out columns that don't exist
+    packet_heights = {}
     timeslot = 0
     for timeslot_ip in timeslot_packets:
         for address in all_addresses:
-            if str(timeslot) not in packet_percentages:
-                packet_percentages[str(timeslot)] = {}
+            if str(timeslot) not in packet_heights:
+                packet_heights[str(timeslot)] = {}
 
             if address in timeslot_ip:
-                packet_percentages[str(timeslot)][address] = timeslot_ip[address] / height
+                packet_heights[str(timeslot)][address] = timeslot_ip[address]
             else:
-                packet_percentages[str(timeslot)][address] = 0
+                packet_heights[str(timeslot)][address] = 0
         timeslot += 1
 
-    return packet_percentages
+    # Format data
+    address_throughputs = {}
+    for timeslot, addresses in packet_heights.items():
+        for address, throughput in addresses.items():
+            if address not in address_throughputs:
+                address_throughputs[address] = {}
+            address_throughputs[address][timeslot] = throughput
+
+    return address_throughputs
 
 # Set variables
-timeslot_packets = [{"192.168.1.1": 1000, "10.2.1.2": 234, "1.1.1.1": 111}, {"192.168.1.1": 90, "10.2.1.2": 234}]
-graph_height = get_graph_height(timeslot_packets)
-
-# get all addresses
-all_addresses = set()
-timeslot_addresses = list(map(lambda packet: [*packet], timeslot_packets))
-for timeslot in timeslot_addresses:
-    for address in timeslot:
-        all_addresses.add(address)
-all_addresses = [*all_addresses]
-
-graph_percentages = get_percentages(all_addresses, timeslot_packets, graph_height)
-
-# Format the data
-address_throughput = {}
-for timeslot, addresses in graph_percentages.items():
-    for address, throughput in addresses.items():
-        if address not in address_throughput:
-            address_throughput[address] = {}
-        address_throughput[address][timeslot] = throughput
+timeslot_packets = [{"192.168.1.1": 1000, "10.2.1.2": 234, "1.1.1.1": 111}, {"192.168.1.1": 90, "10.2.1.2": 234, "10.2.1.3": 100}]
+address_throughputs = get_packet_data(timeslot_packets)
 
 # Plot the data
-df = pd.DataFrame(address_throughput)
+df = pd.DataFrame(address_throughputs)
 df.plot.bar(stacked=True)
 
 plt.show()
