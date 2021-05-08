ef97b9c6ef94224f4361606e7c661d5ee298493d
stilltum <35137975+stilltum@users.noreply.github.com>
2021-05-08 22:28:40 +0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -11,10 +11,22 @@
 import sys
 ifadapter = sys.argv[1]
 
+# Convert IP's to Hostnames
+import socket
+
+ips_to_hostnames = {}
+def add_ip_to_hostnames(ips):
+    for ip in ips.keys():
+        if ip not in ips_to_hostnames:
+            try:
+                ips_to_hostnames[ip] = socket.gethostbyaddr(ip)[0]
+            except socket.herror:
+                ips_to_hostnames[ip] = ip
+
+
 # Graph Printing Code
 import pandas as pd
 import matplotlib.pyplot as plt
-
 
 def print_graph(minute_byte_count):
     
@@ -26,9 +38,10 @@ def print_graph(minute_byte_count):
         address_throughputs = {}
         for timeslot, addresses in labeled_packets:
             for address, throughput in addresses.items():
-                if address not in address_throughputs:
-                    address_throughputs[address] = {}
-                address_throughputs[address][timeslot] = throughput
+                hostname = ips_to_hostnames[address]
+                if hostname not in address_throughputs:
+                    address_throughputs[hostname] = {}
+                address_throughputs[hostname][timeslot] = throughput
 
         return address_throughputs
 
@@ -46,7 +59,6 @@ def print_graph(minute_byte_count):
 # Packet Sniffing Code
 from collections import Counter
 from scapy.all import sniff
-minute_byte_count = []
 
 # Sum bytes sent in each second
 sniff_data = {}
@@ -57,14 +69,17 @@ def sum_packet_lengths(packet):
         sniff_data[packet[0][1].dst] += len(packet)
 
 ## Setup sniff, filtering for IP traffic
+minute_byte_count = []
 for i in range(0, 10):
 
     sniff_data = {}
     sniff(iface=ifadapter, filter="ip", prn=sum_packet_lengths, timeout=1)
 
+    add_ip_to_hostnames(sniff_data)
+
     if len(minute_byte_count) == 60:
         minute_byte_count = minute_byte_count[:-1]
     
     minute_byte_count.insert(0, sniff_data)
- 
+    
     print_graph(minute_byte_count)
